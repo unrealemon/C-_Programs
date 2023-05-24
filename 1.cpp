458f64ac7b53d552a0d2a4ab9974fb03f141df81
Md. Emon Ali <emonali.w@gmail.com>
2023-05-24 23:09:08 +0600

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+// Simple c++ Program
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum = 0;
+
+    cout<<"Enter two number: " ;
+    cin>>a>>b;     // take input like scanf()
+
+   sum = a+b;
+
+   cout<<"Summation is: "<<sum ;
+   cout<<endl;  //For new line
+
+   return 0;
+}
