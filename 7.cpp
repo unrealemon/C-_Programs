a11d80e3131998e1aa14fcb8899d30a738cba638
Md. Emon Ali <emonali.w@gmail.com>
2023-05-24 23:11:06 +0600

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,39 @@
+// Exercise Page - 38 ( 1 )
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int sroot(int a)
+{
+    return sqrt(a);
+}
+
+long int sroot(long int a)
+{
+    return sqrt(a);
+}
+double sroot(double a)
+{
+    return sqrt(a);
+}
+/*
+int sroot(int a)
+{
+    return pow(a,o.5);   // Without sqrt() !
+}
+*/
+
+
+int main()
+{
+    int x = 100 ;
+    cout<<"Sroot For integer number: "<<sroot(x)<<endl;
+
+    long int y = 262144 ;
+    cout<<"Sroot For integer number: "<<sroot(y)<<endl;
+
+    double z = 550 ;
+    cout<<"Sroot For integer number: "<<sroot(z)<<endl;
+
+    return 0;
+}
