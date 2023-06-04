4474dda68c71f66441f3735b0e9052c3b889cdea
Md. Emon Ali <emonali.w@gmail.com>
2023-06-04 23:36:17 +0600

    Add files via upload

---
+++ Inline function without class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+inline int min(int a, int b)
+{
+    return a<b ? a:b;
+}
+inline long min(long a, long b)
+{
+    return a<b? a:b;
+}
+inline double min(double a, double b)
+{
+    return a<b? a:b;
+}
+int main()
+{
+    cout<< min(-10,10) <<endl;
+     cout<< min(-10.01,100.002) <<endl;
+    return 0;
+}
